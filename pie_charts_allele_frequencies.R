d89ba5b94997a6df41517b6fd77deff8b578321d
Paul-bunel <paulbunel34@gmail.com>
2023-03-10 14:52:01 +0100

    Update pie chart R script and map

--- pie_charts_allele_frequencies.R
+++ pie_charts_allele_frequencies.R
@@ -7,10 +7,10 @@ library(htmltools)
 coordinates <- read.csv2("Africaneo_dataset_geographical_coordinates.csv")
 SNPs <- read.table("SNPs_of_interest_analysis_clst.frq.strat", header = T,
                    sep = "\t", colClasses = c(rep("character", 4), rep("numeric", 3)))
-SNP_of_interest <- "1_9115700"
+
+SNP_of_interest <- "7_141687327"
+
 SNP <- subset(SNPs, CODE == SNP_of_interest)
-# SNP <- read.table("retrieved_MGAM_first_SNP.frq.strat", header = T, sep="\t",
-#                   colClasses = c(rep("character", 4), rep("numeric", 3)))
 colnames(SNP)[colnames(SNP) == "MAC"] = "MA1"
 SNP$MA2 <- SNP$NCHROBS - SNP$MA1
 SNP_coordinates <- merge(
@@ -20,7 +20,35 @@ SNP_coordinates <- merge(
   by.y = "Population.label"
 )
 
+dbSNP <- read.csv("dbSNP_retrieved.csv", header = F,
+                  col.names = c("GENE", "RSID", "CHR", "POS", "ALLELES"),
+                  colClasses = c(rep("character", 5)))
+CHR_of_interest <- strsplit(SNP_of_interest, "_")[[1]][1]
+POS_of_interest <- strsplit(SNP_of_interest, "_")[[1]][2]
+GENE_of_interest <- dbSNP$GENE[dbSNP$CHR == CHR_of_interest & dbSNP$POS == POS_of_interest]
+
 chart_datas <- SNP_coordinates[, c("MA1", "MA2")]
+
+tag.map.title <- tags$style(HTML("
+  .leaflet-control.map-title { 
+    transform: translate(-50%,20%);
+    position: fixed !important;
+    left: 50%;
+    text-align: center;
+    padding-left: 10px; 
+    padding-right: 10px; 
+    background: rgba(255,255,255,0.75);
+    font-size: 15px;
+  }
+"))
+
+title <- tags$div(
+  tag.map.title, HTML(paste0(
+    "GENE OF INTEREST : ", GENE_of_interest, "<br>",
+    "CHROMOSOME : ", CHR_of_interest, "<br>",
+    "POSITION : ", POS_of_interest
+  ))
+)  
 
 map <- leaflet() %>%
   addTiles(
@@ -40,9 +68,9 @@ map <- leaflet() %>%
     fillOpacity = 0,
     radius = sqrt(SNP_coordinates$N) / sqrt(max(SNP_coordinates$N)) * 20,
     popup = paste0(
-              "Echantillon : ", SNP_coordinates$N,
-              "<br>", SNP_coordinates$A1[1], " : ", SNP_coordinates$MA1,
-              "<br>", SNP_coordinates$A2[1], " : ", SNP_coordinates$MA2
+              "Echantillon : ", SNP_coordinates$NCHROBS / 2, "<br>",
+              SNP_coordinates$A1[1], " : ", SNP_coordinates$MA1, "<br>",
+              SNP_coordinates$A2[1], " : ", SNP_coordinates$MA2
             )
   ) %>%
   addLegend(
@@ -51,7 +79,8 @@ map <- leaflet() %>%
     colors = d3.schemeCategory10[1:length(chart_datas)],
     opacity = 1,
     layerId = "minichartsLegend",
-  )
+  ) %>%
+  addControl(title, position = "topleft", className="map-title")
 map
 
 saveWidget(map, file="map.html")
